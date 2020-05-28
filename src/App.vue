2cf85171e7cacfbe48d41a71eca52c6e48f167a9
unknown <fubautsu@gmail.com>
2020-05-28 22:36:51 +0200

    Mise en place du menu gauche

--- src/App.vue
+++ src/App.vue
@@ -1,103 +1,111 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated class="glossy">
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          @click="leftDrawerOpen = !leftDrawerOpen"
-          aria-label="Menu"
-          icon="menu"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-2"
-    >
-      <q-list>
-        <q-item-label header>Essential Links</q-item-label>
-        <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="school" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Docs</q-item-label>
-            <q-item-label caption>quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://github.com/quasarframework/">
-          <q-item-section avatar>
-            <q-icon name="code" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Github</q-item-label>
-            <q-item-label caption>github.com/quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://chat.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="chat" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Discord Chat Channel</q-item-label>
-            <q-item-label caption>chat.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://forum.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="forum" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Forum</q-item-label>
-            <q-item-label caption>forum.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://twitter.com/quasarframework">
-          <q-item-section avatar>
-            <q-icon name="rss_feed" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Twitter</q-item-label>
-            <q-item-label caption>@quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
+    <q-drawer bordered show-if-above content-class="bg-grey-2">
+      <div class="row justify-center">
+          <img id="logo" src="./assets/logo.png" />
+        </div>
+        <div class="row justify-center">
+            <div class="col text-center text-h4">
+              <q-icon name="home" />
+            </div>
+            <div class="col text-center text-h4">
+              <q-icon name="search" />
+            </div>
+            <div class="col text-center text-h4">
+              <q-icon name="exit_to_app" />
+            </div>
+        </div>
+        <q-list separator>
+            <q-item>
+              <h4>Mon compte</h4>
+            </q-item>
+            <q-item clickable>
+              <q-item-section avatar>
+                <q-icon name="storage" />
+              </q-item-section>
+              <q-item-section>Ma collection</q-item-section>
+            </q-item>
+            <q-item clickable>
+              <q-item-section avatar>
+                <q-icon name="favorite" />
+              </q-item-section>
+              <q-item-section>Ma wishlist</q-item-section>
+            </q-item>
+            <q-item clickable>
+              <q-item-section avatar>
+                <q-icon name="pan_tool" />
+              </q-item-section>
+              <q-item-section>Mes emprunts</q-item-section>
+            </q-item>
+            <q-item clickable>
+              <q-item-section avatar>
+                <q-icon name="work" />
+              </q-item-section>
+              <q-item-section>Prêts</q-item-section>
+            </q-item>
+            <q-item clickable>
+              <q-item-section avatar>
+                <q-icon name="verified_user" />
+              </q-item-section>
+              <q-item-section>Administration</q-item-section>
+            </q-item>
+        </q-list>
+        <q-separator spaced />
+        <h4>Médiathèque</h4>
+        <q-list separator>
+            <q-item clickable>
+              <q-item-section avatar>
+                <q-icon name="music_note" />
+              </q-item-section>
+              <q-item-section>Audio</q-item-section>
+            </q-item>
+            <q-item clickable>
+              <q-item-section avatar>
+                <q-icon name="movie" />
+              </q-item-section>
+              <q-item-section>Vidéo</q-item-section>
+            </q-item>
+            <q-item clickable>
+              <q-item-section avatar>
+                <q-icon name="menu_book" />
+              </q-item-section>
+              <q-item-section>Livre</q-item-section>
+            </q-item>
+            <q-item clickable>
+              <q-item-section avatar>
+                <q-icon name="gamepad" />
+              </q-item-section>
+              <q-item-section>Jeux</q-item-section>
+            </q-item>
+        </q-list>
     </q-drawer>
-
     <q-page-container>
-      <HelloWorld />
+      <router-view></router-view>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'LayoutDefault',
+  name: "LayoutDefault",
 
-  components: {
-    HelloWorld
-  },
+  components: {},
 
-  data () {
+  data() {
     return {
-      leftDrawerOpen: false
-    }
+      
+    };
   }
-}
+};
 </script>
 
 <style>
+h4 {
+  margin: 10px;
+}
+
+#logo {
+  width: 50%;
+  margin: 10px;
+}
 </style>
