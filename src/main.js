f7846e34cd6b28fd6ac08744bfe9c74561c0295d
unknown <fubautsu@gmail.com>
2020-05-29 18:24:06 +0200

    Ajout de Vuex

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import './quasar'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
